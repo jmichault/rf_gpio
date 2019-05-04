6c1bdee03f24597382dd9931e749e11b180b8676
jmichault <jmichault@users.sourceforge.net>
2019-05-04 11:29:57 +0200

    aldono de protokoloj.h

---
+++ src/protokoloj.h
@@ -0,0 +1,7 @@
+
+void dormi( int mikroSekundoj);
+
+int sendo_p(char * komando, struct sentilo * sentilo);
+int sendo_t(char * komando, struct sentilo * sentilo);
+int testo_p(int saltiKodojn,const char *kodoj,int trakti,struct bufroKadro *bufK);
+int testo_p001(int saltiKodojn,const char *kodoj,int trakti,struct bufroKadro *bufK);
